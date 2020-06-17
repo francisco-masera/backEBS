9574bb69c4455ad464a3486de50105520a315a97
Dargor94 <franciscomasera1994@gmail.com>
2020-06-16 21:04:43 -0300

    issue#25

--- back/src/main/java/ebs/back/controller/ArticuloManufacturadoController.java
+++ back/src/main/java/ebs/back/controller/ArticuloManufacturadoController.java
@@ -1,11 +1,29 @@
 package ebs.back.controller;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import ebs.back.entity.ArticuloManufacturado;
+import ebs.back.entity.Insumo;
+import ebs.back.entity.Receta;
+import ebs.back.entity.Stock;
+import ebs.back.entity.wrapper.ArticuloManufacturadoWrapper;
 import ebs.back.service.ArticuloManufacturadoService;
 
 @RestController
@@ -14,5 +32,83 @@ import ebs.back.service.ArticuloManufacturadoService;
 @RequestMapping(path = "buensabor/manufacturado")
 public class ArticuloManufacturadoController
 		extends BaseController<ArticuloManufacturado, ArticuloManufacturadoService> {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+	/**
+	 * 
+	 * @param idManufacturado
+	 * @return True: Si un Manufacturado puede elaborarse, teniendo en cuenta si hay
+	 *         stock suficiente de sus insumos
+	 */
+	@GetMapping("/stockManufacturado/{id}")
+	public boolean hayStockManufacturado(@PathVariable Long id) {
+
+		ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+		mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+		Object response = this.getOne(id).getBody();
+
+		String responseJson = "";
+		try {
+			responseJson = mapper.writeValueAsString(response);
+		} catch (JsonProcessingException e) {
+
+			e.printStackTrace();
+		}
+
+		ArticuloManufacturadoWrapper manufacturadoWrapper = new ArticuloManufacturadoWrapper();
+		try {
+			manufacturadoWrapper = mapper.readValue(responseJson, ArticuloManufacturadoWrapper.class);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+
+		manufacturadoWrapper.setRecetas(this.getRecetasXManufacturado(id));
+		return this.verificarStock(manufacturadoWrapper);
+	}
+
+	/**
+	 * 
+	 * @param idManufacturado
+	 * @return Todas las recetas de un Manufacturado, con sus Insumos
+	 */
+	public List<Receta> getRecetasXManufacturado(Long idManufacturado) {
+		List<Receta> resultList = this.jdbcTemplate.query(
+				"SELECT r.idReceta, r.cantidadInsumo, i.idInsumo, s.actual "
+						+ "FROM stock s INNER JOIN insumo i ON s.idStock = i.idStock INNER JOIN receta r "
+						+ "ON i.idInsumo = r.idInsumo WHERE r.idManufacturado = " + idManufacturado,
+
+				new RowMapper<Receta>() {
+					@Override
+					public Receta mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Receta receta = new Receta();
+						receta.setId(rs.getLong("r.idReceta"));
+						receta.setCantidadInsumo(rs.getFloat("r.cantidadInsumo"));
+
+						Insumo insumo = new Insumo();
+						insumo.setIdInsumo(rs.getLong("i.idInsumo"));
+
+						Stock stock = new Stock();
+						stock.setActual(rs.getLong("actual"));
+						insumo.setStock(stock);
+						receta.setInsumo(insumo);
+						return receta;
+					}
+				});
+
+		return resultList;
+	}
+
+	/**
+	 * 
+	 * @param manufacturado
+	 * @return True: Si Todos los insumos del manufacturado tienen stock necesario
+	 *         para fabricarlo
+	 */
+	public boolean verificarStock(ArticuloManufacturadoWrapper manufacturado) {
+		return manufacturado.getRecetas().stream()
+				.allMatch(r -> r.getCantidadInsumo() <= r.getInsumo().getStock().getActual());
+	}
 
 }
---
+++ back/src/main/java/ebs/back/entity/wrapper/ArticuloManufacturadoWrapper.java
@@ -0,0 +1,89 @@
+package ebs.back.entity.wrapper;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import ebs.back.entity.Receta;
+import ebs.back.entity.RubroManufacturado;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ArticuloManufacturadoWrapper {
+
+	private int tiempoCocina;
+	private boolean aptoCeliaco;
+	private boolean vegano;
+	private boolean vegetariano;
+	private String denominacion;
+	private boolean baja;
+	private RubroManufacturado rubro;
+	private List<Receta> recetas;
+
+	public ArticuloManufacturadoWrapper() {
+	}
+
+	public int getTiempoCocina() {
+		return tiempoCocina;
+	}
+
+	public void setTiempoCocina(int tiempoCocina) {
+		this.tiempoCocina = tiempoCocina;
+	}
+
+	public boolean isAptoCeliaco() {
+		return aptoCeliaco;
+	}
+
+	public void setAptoCeliaco(boolean aptoCeliaco) {
+		this.aptoCeliaco = aptoCeliaco;
+	}
+
+	public boolean isVegano() {
+		return vegano;
+	}
+
+	public void setVegano(boolean vegano) {
+		this.vegano = vegano;
+	}
+
+	public boolean isVegetariano() {
+		return vegetariano;
+	}
+
+	public void setVegetariano(boolean vegetariano) {
+		this.vegetariano = vegetariano;
+	}
+
+	public String getDenominacion() {
+		return denominacion;
+	}
+
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+
+	public boolean isBaja() {
+		return baja;
+	}
+
+	public void setBaja(boolean baja) {
+		this.baja = baja;
+	}
+
+	public RubroManufacturado getRubro() {
+		return rubro;
+	}
+
+	public void setRubro(RubroManufacturado rubro) {
+		this.rubro = rubro;
+	}
+
+	public List<Receta> getRecetas() {
+		return recetas;
+	}
+
+	public void setRecetas(List<Receta> recetas) {
+		this.recetas = recetas;
+	}
+
+}
