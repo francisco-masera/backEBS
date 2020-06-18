4bcf00b235955b6e9bb3ec3500ba95bf5fb41a3f
Dargor94 <franciscomasera1994@gmail.com>
2020-06-18 09:49:52 -0300

    issue#32

--- back/src/main/java/ebs/back/controller/InsumoController.java
+++ back/src/main/java/ebs/back/controller/InsumoController.java
@@ -1,9 +1,11 @@
 package ebs.back.controller;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,22 +22,19 @@ import ebs.back.service.InsumoService;
 @RequestMapping(path = "buensabor/insumo")
 public class InsumoController extends BaseController<Insumo, InsumoService> {
 
-	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
-
 	@Override
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id) {
+	@Modifying(clearAutomatically = true)
+	@Query("UPDATE insumo SET baja = 1 WHERE idInsumo =: id")
+	public ResponseEntity<?> delete(@Param("id") @PathVariable Long id) {
 		try {
-			jdbcTemplate.update("UPDATE insumo SET baja = 1 WHERE idInsumo = " + id);
 			return ResponseEntity.status(HttpStatus.OK).body(service.delete(id));
-
 		} catch (DataAccessException e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body("{\"Error interno del servidor\": \"" + e.getMessage() + "\"}");
 		} catch (IllegalArgumentException e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 					.body("{\"Error en la solicitud\": \"" + e.getMessage() + "\"}");
-
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND)
 					.body("{\"Error inesperado\": \"" + e.getMessage() + "\"}");
