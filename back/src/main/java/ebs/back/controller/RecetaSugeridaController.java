ddbbbe13e3a04058d5f436adbc234ec05dfac362
Dargor94 <franciscomasera1994@gmail.com>
2020-08-07 14:17:22 -0300

    issue #50

--- back/src/main/java/ebs/back/controller/RecetaSugeridaController.java
+++ back/src/main/java/ebs/back/controller/RecetaSugeridaController.java
@@ -1,6 +1,13 @@
 package ebs.back.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,4 +21,26 @@ import ebs.back.service.RecetaSugeridaService;
 @RequestMapping(path = "buensabor/recetaSugerida")
 public class RecetaSugeridaController extends BaseController<RecetaSugerida, RecetaSugeridaService> {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+	@Override
+	public ResponseEntity<?> save(RecetaSugerida recetaSugerida) {
+		try {
+			jdbcTemplate.update("INSERT INTO recetasugerida (cantidadInsumo, idInsumo, idSugerencia) VALUES (?, ?, ?)",
+					recetaSugerida.getCantidadInsumo(), recetaSugerida.getInsumo().getIdInsumo(),
+					recetaSugerida.getSugerenciaChef().getId());
+			return ResponseEntity.status(HttpStatus.OK).body(recetaSugerida);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
+	}
+
+	@PostMapping("/guardarReceta/{idSugerencia}")
+	public ResponseEntity<?> auxPostReceta(@RequestBody RecetaSugerida receta, @PathVariable Long idSugerencia) {
+		receta.getSugerenciaChef().setId(idSugerencia);
+		return this.save(receta);
+	}
 }
