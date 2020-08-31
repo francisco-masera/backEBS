35e0b06084f2a3b6aeb74c9a4342f4104072523c
rooespinoza <rocio.espinoza@alumnos.frm.utn.edu.ar>
2020-08-31 00:07:08 -0300

    issue#55

--- back/src/main/java/ebs/back/service/HistorialCompraAProveedoresService.java
+++ back/src/main/java/ebs/back/service/HistorialCompraAProveedoresService.java
@@ -8,5 +8,18 @@ import ebs.back.repository.HistorialCompraAProveedoresRepository;
 @Service
 public class HistorialCompraAProveedoresService
 		extends BaseService<HistorialCompraAProveedores, HistorialCompraAProveedoresRepository> {
+	
+	@Override
+	public HistorialCompraAProveedores save(HistorialCompraAProveedores entity) throws Exception {
+		try {
+			return repository.save(entity);
+		
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+	}
 
 }
