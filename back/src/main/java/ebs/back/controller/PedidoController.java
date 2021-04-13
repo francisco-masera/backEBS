b6f28801dd55c30a5f66f0503615acb766001dfb
Georgina <georgi888111@gmail.com>
2021-04-12 23:17:07 -0300

    Avances pedido delivery backend

--- back/src/main/java/ebs/back/controller/PedidoController.java
+++ back/src/main/java/ebs/back/controller/PedidoController.java
@@ -1,36 +1,35 @@
 package ebs.back.controller;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import ebs.back.entity.Cliente;
-import ebs.back.entity.Domicilio;
 import ebs.back.entity.Pedido;
-import ebs.back.entity.Persona;
-import ebs.back.entity.Receta;
 import ebs.back.service.PedidoService;
 
 @RestController
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
-		RequestMethod.DELETE })
+        RequestMethod.DELETE })
 @RequestMapping(path = "buensabor/pedido")
 public class PedidoController extends BaseController<Pedido, PedidoService> {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+    @Autowired
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
+    @RequestMapping(value = "/pedidoEntregado/{id}/{estado}", method = RequestMethod.PUT, produces = {
+            MediaType.APPLICATION_JSON_UTF8_VALUE })
+
+    public int updatePedidoEntregado(@PathVariable Long id, @PathVariable String estado) {
+        int estadoPedido;
+        System.out.print(estado);   
+        System.out.print("Update pedido set estado = " + "'" + estado + "'" + "where idPedido = " + id); 
+        return estadoPedido = jdbcTemplate.update("Update pedido set estado = " + "'" + estado + "'" + "where idPedido = " + id);
+    }
 
 }
